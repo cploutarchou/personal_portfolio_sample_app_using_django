28bb5bdeadc2ec0c9ef378462243a458568f4097
Christos Ploutarchou <cploutarchou@gmail.com>
2020-07-03 00:04:49 +0300

    update urls.py #24

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('', views.category, name='category')
 ]
