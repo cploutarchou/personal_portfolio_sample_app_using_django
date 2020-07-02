32b875d88f50ee1b0a252577f6d4c0e274a9452c
Christos Ploutarchou <cploutarchou@gmail.com>
2020-07-02 18:54:23 +0300

    rename app project to blog  #24

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["app/templates"],
+        'DIRS': ["blog/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
