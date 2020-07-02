a2d252dac055c7880e0c453d5652343f9b081c1d
Christos Ploutarchou <cploutarchou@gmail.com>
2020-07-03 00:04:29 +0300

    update views.py #24

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+import datetime
+
+from django.shortcuts import render, get_object_or_404
 
 
 # Create your views here.
 def index(request):
-    return render(request, "default_template/index.html", {})
+    return render(request, "default_template/index.html", {
+        'current_year': datetime.datetime.now(),
+    })
+
+
+def category(request):
+    return render(request, "default_template/category.html", {
+        'current_year': datetime.datetime.now(),
+    })
