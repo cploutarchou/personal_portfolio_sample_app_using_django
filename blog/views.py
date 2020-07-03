1f582c67429a8fb287e7caaeae37829c88dc5896
Christos Ploutarchou <cploutarchou@gmail.com>
2020-07-03 21:47:43 +0300

    add about view

--- blog/views.py
+++ blog/views.py
@@ -20,3 +20,9 @@ def single(request):
     return render(request, "default_template/single.html", {
         'current_year': datetime.datetime.now(),
     })
+
+
+def about(request):
+    return render(request, "default_template/about.html", {
+        'current_year': datetime.datetime.now(),
+    })
