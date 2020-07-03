98aab48b3c90d46eff51fecbe98b36a2a0b55095
christos <cploutarchou@gmail.com>
2020-07-03 21:38:35 +0300

    Update urls and views files #24

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from blog import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('category', views.category, name='category')
+    path('category', views.category, name='category'),
+    path('category', views.category, name='single')
 ]
