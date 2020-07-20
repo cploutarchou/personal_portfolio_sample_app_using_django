420f0205044570170ba6666a4dd3b2e313732deb
Christos Ploutarchou <cploutarchou@gmail.com>
2020-07-20 22:17:54 +0300

    update models.py

--- cms/models.py
+++ cms/models.py
@@ -23,8 +23,8 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
-    objects = models.Manager()  # The default manager.
-    published = PublishedManager()  # The Dahl-specific manager.
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
