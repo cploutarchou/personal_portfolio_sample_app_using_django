0b3063546deb5dec976a2e131f09406fbcbe68cb
Christos Ploutarchou <cploutarchou@gmail.com>
2020-07-20 22:38:31 +0300

    create views.py

--- cms/views.py
+++ cms/views.py
@@ -1,7 +1,16 @@
+import datetime
+
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from cms.models import Post
+
+
+def index(request):
+    return render(request, "cms/base.html", {
+        'current_year': datetime.datetime.now(),
+        'title': 'Home Page'
+    })
 
 
 def post_list(request, category=None):
