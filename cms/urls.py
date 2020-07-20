e07151b6268170da7d126ce076bd0d0ed6c19049
Christos Ploutarchou <cploutarchou@gmail.com>
2020-07-20 22:38:02 +0300

    update urls.py

--- cms/urls.py
+++ cms/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
-from . import views
-
+from django.urls import path
+from cms import views
 
 urlpatterns = [
-    # post views
-    url(r'^$', views.PostListView.as_view(), name='post_list'),
+    path('', views.index, name='index'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',
         views.post_detail,
         name='post_detail'),
