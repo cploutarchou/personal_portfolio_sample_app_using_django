9bf8aa59a071810c6c53b7acdd433712cce4c21f
Christos Ploutarchou <c.ploutarchou@unicaf.org>
2020-12-21 14:45:56 +0100

    Update app models.py

--- app/models.py
+++ app/models.py
@@ -3,20 +3,19 @@ from datetime import datetime
 from django.db import models
 
 
-# Create your models here.
-class Menu(models.Model):
-    parent_id = models.IntegerField(3)
-    title = models.CharField(max_length=100)
-    position = models.IntegerField(2)
-    group_id = models.IntegerField(5)
-    created_at = models.DateTimeField(datetime.now())
-    prefix = models.CharField(max_length=250)
-    order = models.IntegerField(2)
-    active = models.BooleanField("True")
-
-
 class MenuGroup(models.Model):
     title = models.CharField(max_length=10)
+
+
+class Menu(models.Model):
+    parent_id = models.SmallIntegerField(3)
+    title = models.CharField(max_length=100)
+    position = models.SmallIntegerField(2)
+    group_id = models.ForeignKey(MenuGroup, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(datetime.now())
+    prefix = models.CharField(max_length=250)
+    order = models.SmallIntegerField(2)
+    active = models.BooleanField("True")
 
 
 class Posts(models.Model):
