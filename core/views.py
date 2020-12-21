6e31cf7d7862dbd2fc21e0764f53e5e39c232a6e
Christos Ploutarchou <c.ploutarchou@unicaf.org>
2020-12-21 13:38:21 +0100

    Create cms app

--- core/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'default/base.html', {})
---
+++ front_end/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .models import *
+
+
+def index(request):
+    main_menu = MainMenu.objects.all()
+    context = {
+        "main_menu_items": main_menu
+    }
+    return render(request, 'default/base.html', context)
+
+
+def posts(request):
+    return render(request, 'default/posts.html', {})
+
+
+def post(request, pk):
+    _post = Posts.objects.get(pk=pk)
+    context = {
+        "post": _post
+    }
+    return render(request, 'default/post.html', context)
