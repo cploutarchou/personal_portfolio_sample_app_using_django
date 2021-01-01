587c72764032d7e762af799168e37d9801996d71
Christos Ploutarchou <cploutarchou@gmail.com>
2021-01-01 16:22:30 +0200

    Update posts class

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Menu, MenuGroup
+from .models import Menu, MenuGroup, Posts
 
 admin.site.register(Menu)
 admin.site.register(MenuGroup)
+admin.site.register(Posts)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from datetime import datetime
+from django.utils.timezone import localtime, now
 from django.db import models
+from datetime import timezone, timedelta
 
 
 class MenuGroup(models.Model):
@@ -11,7 +12,7 @@ class Menu(models.Model):
     title = models.CharField(max_length=100)
     position = models.SmallIntegerField(2)
     group_id = models.ForeignKey(MenuGroup, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(default=datetime.now())
+    created_at = models.DateTimeField(localtime)
     prefix = models.CharField(max_length=250)
     order = models.SmallIntegerField(2)
     active = models.BooleanField(default=True)
@@ -21,3 +22,22 @@ class Settings(models.Model):
     name = models.CharField(max_length=250, unique=True)
     value = models.TextField()
     active = models.BooleanField(default=True, db_index=True)
+
+
+class Posts(models.Model):
+    author = models.BigIntegerField(default=0)
+    submitted_date = models.DateTimeField(name="Status")
+    context = models.TextField()
+    title = models.CharField(max_length=150)
+
+    class Status(models.TextChoices):
+        PUB = '1', "Publish"
+        SAVE = '2', "Save"
+        DRAFT = '3', "Draft"
+        PUBED = '4', "Published"
+
+    month = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
